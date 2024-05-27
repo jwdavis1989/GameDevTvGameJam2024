b5bf87a9360c48270c4ad51e724a014c98f1cb32
jwdavis1989 <jwdavis8589@gmail.com>
2024-05-26 19:35:09 -0500

    Merged GameController, GameManager, and BuildManager prefabs.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,13 +8,27 @@ public class GameController : MonoBehaviour
     public List<GameObject> aisles;
     public bool SpawnMode;
     public bool BuildMode;
-    public int managerWriteUps = 0;
     public TextMeshProUGUI managerWriteUpText;
+    public static GameController instance;
+
+    [Header("Player Global Attributes")]
+    public int managerWriteUps = 0;
+    public static int money;
+    public int startingMoney = 2;
+
+    void Awake() {
+        if (instance) {
+            return;
+        }
+            instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         managerWriteUps = 0;
         managerWriteUpText.text = "Manager Write-Ups " + managerWriteUps + " / 5";
+        money = startingMoney;
     }
 
     // Update is called once per frame
