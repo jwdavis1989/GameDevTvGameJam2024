38b917a5a288a6ea4e1813909ec13cd9ae428916
Alec Straw <alec.straw.1@gmail.com>
2024-05-27 18:41:45 -0500

    make aisle object

---
+++ Assets/Scripts/Aisles/AisleController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AisleController : MonoBehaviour
+{
+    public int aisleNumber = 0;
+    public bool isActive = true;
+    public GameObject WestMarker;
+    public AisleMarker WestMarkerScript;
+    public GameObject EastMarker;
+    public AisleMarker EastMarkerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
