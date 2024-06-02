09b29fb0b9682a8d9703a4b78e2418a3ffaafc0f
jwdavis1989 <jwdavis8589@gmail.com>
2024-06-02 16:35:34 -0500

    Rebalanced Turret Sound Levels to Non-Ear-Shattering Levels.

---
+++ Assets/TitleScreenController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenController : MonoBehaviour
+{
+    // public Image[] frames; 
+    // int framesPerSecond = 3;
+    // int currentFrame = 0;
+    // public Image imageRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //imageRef = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+
+    void Update() { 
+        //currentFrame = (Time.time * framesPerSecond) % frames.Length; 
+        //imageRef.image = frames[currentFrame]; 
+    }
+}
